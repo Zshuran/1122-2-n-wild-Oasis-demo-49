e111b8aa8a8ef0b8c55aa6f91de3f41744ac6778
Zshuran <a86619355@gmail.com>
2024-06-12 20:30:05 +0800

    data

--- src/pages/Dashboard_49.jsx
+++ src/pages/Dashboard_49.jsx
@@ -1,5 +1,14 @@
 const Dashboard_49 = () => {
-  return <>Dashboard_49 page</>;
+  return (
+    <>
+      {' '}
+      <strong><h1>Final-2 Exam of Web Prog 2,2024</h1></strong>
+      <p>Guest name: 張冠雲
+         Guest student ID:209100949
+         Supabase email:209100949@gmail.com 
+         Supabase userid:abbc4dd2-b3ea-435c-877b-1801a1e66fac</p>
+    </>
+  );
 };
 
 export default Dashboard_49;
